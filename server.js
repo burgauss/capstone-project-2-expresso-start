2adcc0275d45b57eca0ced1cc4e84318996cdca0
Juan Burgos <burgos.135@gmail.com>
2024-10-20 19:35:47 -0600

    added error handler

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const apiRouter = require('./api/api.js');
-
+const errorHandler = require('./errorhandler/errorhandler.js');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,9 +14,10 @@ app.use(morgan('dev'));
 
 app.use('/api', apiRouter);
 
+app.use(errorHandler)
+
 app.listen(PORT, () =>{
     console.log(`Sever is listening on port ${PORT}`);
   });
-
 
 module.exports = app;
